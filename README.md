cc97fc874668a6be2c82b08efb3abcb697df70f1
Abilovv599 <abilovv599@gmail.com>
2023-06-02 13:23:25 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Forkio-Step-Project 
+
+## Tasks for Kamal
+
+Design the header of the website with the top menu, including the dropdown menu for small screen resolutions.
+
+Design the “People Are Talking About Fork” section
+
+
+## Tasks for Jeyhun
+
+Design the “Revolutionary Editor” block. The buttons should resemble the ones shown in the top right corner (you can take all the SVG icons from there and download the
+corresponding styles used on GitHub).
+
+Design the “Here is what you get” section.
+
+Design the “Fork Subscription Pricing” section. The third element in the price block should always be "highlighted" and larger than the others (not on click/hover, but
+statically).
+
+
+## Used technologies
+
+HTML, CSS, SCSS, JS, Gulp.
---
+++ client/gulpfile.js
@@ -0,0 +1,77 @@
+// IMPORTS
+
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const minify = require("gulp-minify");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify-es").default;
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+
+// FUNCTIONS
+
+function concatHtml() {
+  return gulp
+    .src("./src/*.html")
+    .pipe(concat("index.html"))
+    .pipe(gulp.dest("../"))
+    .pipe(browserSync.stream());
+}
+
+function style() {
+  return gulp
+    .src("./src/assets/sass/**/*.scss")
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .on("error", sass.logError)
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("../assets/css/"))
+    .pipe(browserSync.stream());
+}
+
+function minifyJs() {
+  return gulp
+    .src("./src/assets/js/*.js")
+    .pipe(minify())
+    .pipe(uglify())
+    .pipe(gulp.dest("../assets/js/"))
+    .pipe(browserSync.stream());
+}
+
+function imageMin() {
+  gulp
+    .src("src/assets/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("../assets/images"));
+}
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "../",
+    },
+  });
+  gulp.watch("./src/assets/sass/**/*.scss", style);
+  gulp.watch("./src/*.html", concatHtml);
+  gulp.watch("./src/assets/js/*.js", minifyJs);
+}
+
+// GULP TASKS
+
+gulp.task("concat-html", concatHtml);
+gulp.task("image-min", imageMin);
+gulp.task("compile-sass", style);
+gulp.task("minify-js", minifyJs);
+gulp.task("serve", serve);
+gulp.task(
+  "default",
+  gulp.parallel(
+    "image-min",
+    "concat-html",
+    "compile-sass",
+    "minify-js",
+    "serve"
+  )
+);
